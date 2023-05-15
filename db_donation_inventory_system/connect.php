0e30fce2fc5684dc569c212e30f7d798f495fa1b
Jerahmeel Francisco <108663786+FrancJerah@users.noreply.github.com>
2023-05-15 21:57:42 +0800

    Delete db_donation_inventory_system directory

--- db_donation_inventory_system/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('host', 'localhost');
-define('user', 'root');
-define('pass', '');
-define('db', 'db_donation_inventory_system');
-
-$conn = mysqli_connect(host, user, pass, db) or die('Unable to Connect');
-?>
